5923c2a8548f9c05d4a56732437de1fa408cb41c
Bjørn Olav <bo@jalborg.com>
2024-03-29 08:56:39 +0100

    initial commit
    includes pages about bitcoin, svindel, skatt and oppbevaring

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hjelpesider for bitcoin i Norge
+
+Dette prosjektet er hele Norges hjelpesider for bitcoin, med tilhørende hjelp for vekslingstjenesten Bare Bitcoin.
+
+Informasjonen er delt i to. 
+
+## 1. Om bitcoin
+Denne delen inneholder info om bitcoin, der formålet er å gjøre Norges folk litt mer opplyst om bitcoin. Det kan være oppbevaring av bitcoin, når du burde kjøpe, og hvordan du kan beskytte deg mot svindel.
+
+Om noe kan skrives bedre, eller annerledes, åpne en PR! Det er høy sjanse for at endringene blir akseptert.
+
+## 2. Om vekslingstjenesten Bare Bitcoin
+Denne delen inneholder informasjon om vekslingstjenesten Bare Bitcoin, spesielt retta mot hvordan gjøre ditt og datt, forventninger til hastighet etc.
---
+++ theme.config.tsx
@@ -0,0 +1,111 @@
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  head: () => {
+    const { asPath } = useRouter();
+
+    return (
+      <>
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://barebitcoin.no/favicon.ico"
+        />
+        <meta name="og:title" content="Hjelpesider for Bare Bitcoin" />
+
+        <meta // TODO: dynamic per article?
+          name="og:description"
+          content="Lær om bitcoin, og bruk av vekslingstjenesten Bare Bitcoin. Se svar på ofte stilte spørsmål, og få innspill til sikker og trygg bruk."
+        />
+
+        <meta property="og:locale" content="no_NB" />
+
+        <meta
+          property="og:type"
+          content={
+            asPath === "/"
+              ? "website" // root is a complete website
+              : "article" // everything else is an article
+          }
+        />
+
+        <meta
+          // TODO: make this dynamic
+          name="og:image" // Default image for ALL sites image
+          content="https://barebitcoin.no/social-logo.png"
+        />
+
+        <meta
+          name="twitter:image" // Twitter image
+          content="https://barebitcoin.no/social-logo.png"
+        />
+
+        <Script src="https://sa.bb.no/latest.js" />
+        {/* https://docs.simpleanalytics.com/automated-events */}
+        <Script src="https://sa.bb.no/auto-events.js" />
+        <noscript>
+          {/* eslint-disable @next/next/no-img-element */}
+          <img
+            src="https://sa.bb.no/noscript.gif"
+            alt=""
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </noscript>
+      </>
+    );
+  },
+  logo: <span>Bare Bitcoin</span>,
+  chat: {
+    link: "https://discord.gg/qMhQvVbUeH",
+  },
+  project: { link: "https://github.com/barebitcoin/hjelp" },
+  docsRepositoryBase: "https://github.com/barebitcoin/hjelp/blob/master",
+  footer: {
+    text: "Bare Bitcoin",
+  },
+  search: {
+    placeholder: "Søk i dokumentasjon",
+  },
+  editLink: {
+    text: "",
+  },
+  toc: {
+    title: "På denne siden",
+  },
+  feedback: {
+    content: "Spørsmål? Gi oss feedback",
+    useLink: () => "https://github.com/barebitcoin/hjelp/discussions",
+  },
+
+  // https://nextra.site/docs/docs-theme/theme-configuration#seo-options
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - Bare Bitcoin",
+    };
+  },
+  themeSwitch: {
+    useOptions() {
+      return {
+        dark: "Mørk",
+        light: "Lys",
+        system: "Standard",
+      };
+    },
+  },
+  gitTimestamp: ({ timestamp }) => (
+    <>
+      Sist oppdatert{" "}
+      <time dateTime={timestamp.toISOString()}>
+        {timestamp.toLocaleDateString("no-NB", {
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+        })}
+      </time>
+    </>
+  ),
+};
+
+export default config;
